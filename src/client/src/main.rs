ee173e4014e9b99adbb90df3857eebfffdebdbed
Lucas <luislucas0707@gmail.com>
2022-10-11 17:16:04 +0100

    put/get/subscribe/unsubscribe

--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -6,23 +6,46 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::env;
 
-fn atoi(s: &str) -> i64 {
-    s.parse().unwrap()
-}
-
-fn connect(addr: &str) -> zmq::Socket {
+fn connect(id: u8, addr: &str) -> zmq::Socket {
     let context = zmq::Context::new();
     let socket = context.socket(zmq::SUB).unwrap();
+    socket.set_identity(&[id]).unwrap();
     assert!(socket.connect(addr).is_ok());
     socket
 }
 
+fn message_to_server(socket: &zmq::Socket, message: &str) -> String {
+    socket.send(message, 0).unwrap();
+    let response = socket.recv_string(0).unwrap().unwrap();
+    response
+}
+
+fn put(socket: &zmq::Socket, key: &str, value: &str) -> String {
+    let message = format!("PUT {} {}", key, value);
+    message_to_server(socket, &message)
+}
+
+fn get(socket: &zmq::Socket, key: &str) -> String {
+    let message = format!("GET {}", key);
+    message_to_server(socket, &message)
+}
+
+fn subscribe(socket: &zmq::Socket, key: &str) -> String {
+    let message = format!("SUBSCRIBE {}", key);
+    message_to_server(socket, &message)
+}
+
+fn unsubscribe(socket: &zmq::Socket, key: &str) -> String {
+    let message = format!("UNSUBSCRIBE {}", key);
+    message_to_server(socket, &message)
+}
+
 // put [-m/-f] <message/file> <topic_name>
-// get [-m/-f] <message/file> <topic_name>
+// get [-d/-f] (file) <topic_name>
 // subscribe <topic_name>
 // unsubscribe <topic_name>
 
-fn parse_put_get(args: Vec<String>) -> (String, String){
+fn parse_put(args: Vec<String>) -> (String, String){
     if args.len() != 5 {
         panic!("incorrect number of arguments.\nUsage: put [-m/-f] <message/file> <topic_name>");
     }
@@ -53,6 +76,27 @@ fn parse_put_get(args: Vec<String>) -> (String, String){
     (msg, topic)
 }
 
+fn parse_get(args: Vec<String>) -> (String, String){
+    if args.len() != 5 {
+        panic!("incorrect number of arguments.\nUsage: get [-m/-f] <message/file> <topic_name>");
+    }
+    let msg_store = String::from(&args[2]);
+    let path;
+    match msg_store.as_str() {
+        "-d" => {
+            path = String::from("");
+        },
+        "-f" => {
+            path = String::from(&args[3]);
+        },
+        _ => {
+            panic!("incorrect flag.\nUsage: get [-d/-f] (file) <topic_name>");
+        }
+    }
+    let topic = String::from(&args[4]);
+    (path, topic)
+}
+
 fn parse_subscribe_unsubscribe(args: Vec<String>) -> String {
     if args.len() != 3 {
         panic!("incorrect number of arguments.\nUsage: subscribe <topic_name>");
@@ -62,17 +106,17 @@ fn parse_subscribe_unsubscribe(args: Vec<String>) -> String {
 
 fn parse_args(args: Vec<String>) -> (String, String, String) {
     if args.len() < 2 || args.len() > 5 {
-        panic!("Usage:\n  put [-m/-f] <message/file> <topic_name>\n  get <topic_name>\n  subscribe <topic_name>\n  unsubscribe <topic_name>");
+        panic!("Usage:\n  put [-m/-f] <message/file> <topic_name>\n  get [-d/-f] (file) <topic_name>\n  subscribe <topic_name>\n  unsubscribe <topic_name>");
     }
     let mode = String::from(&args[1]);
     match mode.as_str() {
         "put" => {
-            let (msg, topic) = parse_put_get(args);
+            let (msg, topic) = parse_put(args);
             (mode, msg, topic)
         }
         "get" => {
-            let (msg, topic) = parse_put_get(args);
-            (mode, msg, topic)
+            let (path, topic) = parse_get(args);
+            (mode, path, topic)
         }
         "subscribe" => {
             let topic = parse_subscribe_unsubscribe(args);
@@ -83,12 +127,42 @@ fn parse_args(args: Vec<String>) -> (String, String, String) {
             (mode, String::from(""), topic)
         }
         _ => {
-            panic!("Usage:\n  put [-m/-f] <message/file> <topic_name>");
+            panic!("Usage:\n  put [-m/-f] <message/file> <topic_name>\n  get [-d/-f] (file) <topic_name>\n  subscribe <topic_name>\n  unsubscribe <topic_name>");
         }
     }
 }
 
+fn store_msg_file(msg: &str, file_path: &str) {
+    let path = Path::new(file_path);
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", path.display(), why),
+        Ok(file) => file,
+    };
+    match file.write_all(msg.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", path.display(), why),
+        Ok(_) => println!("successfully wrote to {}", path.display()),
+    }
+}
+
 fn main() {
+    let socket = connect(1, "tcp://localhost:5556");
     let args: Vec<String> = env::args().collect();
-    parse_args(args);
+    let (mode, msg, topic) = parse_args(args);
+    match mode.as_str() {
+        "put" => {
+            put(&socket, &topic, &msg);
+        }
+        "get" => {
+            get(&socket, &topic);
+        }
+        "subscribe" => {
+            subscribe(&socket, &topic);
+        }
+        "unsubscribe" => {
+            unsubscribe(&socket, &topic);
+        }
+        _ => {
+            panic!("Usage:\n  put [-m/-f] <message/file> <topic_name>");
+        }
+    }
 }
